8b81e129ffea28cd15080e1e46e30d1094b22d8c
Armand G <g.armand2000@gmail.com>
2023-10-18 16:22:36 +0200

    add main template

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main():
+    """ Main program """
+    # Code goes over here.
+    return 0
+
+
+def optimalgaingld(N, K, W, L , CR, CT):
+    pass
+
+def play_gld(N, K, W, L , CR, CT):
+    pass
+
+if __name__ == "__main__":
+    main()
